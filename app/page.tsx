1b710024fcae85cee7e239fd127ef2443de055c5
Md Christien <mdchristien12@gmail.com>
2024-10-26 05:13:09 +0100

    feat: Build the Banner component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import Banner from "./components/sections/Banner/Banner";
 
 export default function Home() {
   return (
-    <main> Hello Next !!</main>
+    <div className="bg-gray-100">
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+        <Banner />
+        {/* products */}
+        {/* <ProductFeed /> */}
+      </main>
+    </div>
   );
 }

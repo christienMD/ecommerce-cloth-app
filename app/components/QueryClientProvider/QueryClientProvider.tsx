fc5c7dd4704086b5ba4b4c6e0dcf07aa577531b1
Md Christien <mdchristien12@gmail.com>
2024-11-07 01:06:20 +0100

    add react query configrations

--- app/components/QueryClientProvider/QueryClientProvider.tsx
+++ app/components/QueryClientProvider/QueryClientProvider.tsx
@@ -9,9 +9,9 @@ import { PropsWithChildren } from "react";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60 * 60,
-      refetchOnWindowFocus: false,
-      refetchOnMount: false
+      staleTime: 1000 * 60 * 5,
+      cacheTime: 1000 * 60 * 10,
+      retry: 3,
     },
   },
 });
